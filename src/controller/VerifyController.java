9c682b15f304eccd70ff1a89a33874c1dde6c533
inperium <ibow9416@740s-pl-111.local>
2017-02-22 13:41:40 -0700

    Created Framework for the Verify Project

--- src/controller/VerifyController.java
+++ src/controller/VerifyController.java
@@ -1,8 +1,13 @@
 package controller;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import view.VerifyFrame;
 
 public class VerifyController {
+	
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 	
 	private VerifyFrame verifyFrame;
 
@@ -13,4 +18,9 @@ public class VerifyController {
 	public void start() {
 
 	}
+	
+	public boolean validateEmail(String emailStr) {
+		        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+		        return matcher.find();
+		}
 }
--- src/view/VerifyPanel.java
+++ src/view/VerifyPanel.java
@@ -2,12 +2,18 @@ package view;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import controller.VerifyController;
 
-public class VerifyPanel {
+public class VerifyPanel extends JPanel
+{
 	private VerifyController baseController;
+	
+	private SpringLayout springLayout;
+	
 	private JLabel firstNameLabel;
 	private JTextField firstName;
 	private JLabel lastNameLabel;
@@ -21,31 +27,63 @@ public class VerifyPanel {
 	public VerifyPanel(VerifyController baseController) {
 		super();
 		this.baseController = baseController;
+		this.springLayout = new SpringLayout();
 		firstNameLabel = new JLabel("First Name");
+		firstName = new JTextField();
 		lastNameLabel = new JLabel("Last Name");
-		phoneNumberLabel = new JLabel("Phone Number Label");
-		firstNameLabel = new JLabel("First Name");
-		
+		lastName = new JTextField();
+		phoneNumberLabel = new JLabel("Phone Number");
+		phoneNumber= new JTextField();
+		emailLabel = new JLabel("Email");
+		email = new JTextField();
+		submit = new JButton("Submit");
 
 		setupPanel();
 		setupLayout();
 		setupListners();
 
 	}
+	
+	private void setupPanel() {
+		this.setLayout(springLayout);
+		this.add(firstNameLabel);
+		this.add(firstName);
+		this.add(lastNameLabel);
+		this.add(lastName);
+		this.add(phoneNumberLabel);
+		this.add(phoneNumber);
+		this.add(emailLabel);
+		this.add(email);
+		this.add(submit);
+	}
 
 	private void setupListners() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	private void setupLayout() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void setupPanel() {
-		// TODO Auto-generated method stub
-		
+		springLayout.putConstraint(SpringLayout.NORTH, firstNameLabel, 49, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, firstNameLabel, 47, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, firstName, 44, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, firstName, -71, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, lastNameLabel, 16, SpringLayout.SOUTH, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lastNameLabel, 0, SpringLayout.WEST, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.NORTH, lastName, 6, SpringLayout.SOUTH, firstName);
+		springLayout.putConstraint(SpringLayout.WEST, firstName, 0, SpringLayout.WEST, lastName);
+		springLayout.putConstraint(SpringLayout.EAST, lastName, -71, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, phoneNumberLabel, 16, SpringLayout.SOUTH, lastNameLabel);
+		springLayout.putConstraint(SpringLayout.EAST, phoneNumberLabel, 0, SpringLayout.EAST, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.NORTH, phoneNumber, 6, SpringLayout.SOUTH, lastName);
+		springLayout.putConstraint(SpringLayout.WEST, lastName, 0, SpringLayout.WEST, phoneNumber);
+		springLayout.putConstraint(SpringLayout.EAST, phoneNumber, -71, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, emailLabel, 16, SpringLayout.SOUTH, phoneNumberLabel);
+		springLayout.putConstraint(SpringLayout.EAST, emailLabel, 0, SpringLayout.EAST, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.NORTH, email, 6, SpringLayout.SOUTH, phoneNumber);
+		springLayout.putConstraint(SpringLayout.WEST, email, 39, SpringLayout.EAST, emailLabel);
+		springLayout.putConstraint(SpringLayout.EAST, email, -71, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.WEST, phoneNumber, 0, SpringLayout.WEST, email);
+		springLayout.putConstraint(SpringLayout.NORTH, submit, 44, SpringLayout.SOUTH, email);
+		springLayout.putConstraint(SpringLayout.WEST, submit, 175, SpringLayout.WEST, this);
 	}
 
 }
